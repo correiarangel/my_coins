161556ca07a9cd51bc3bd8af1f8772365c41b463
correiarangel <correiarangel@bol.com.br>
2021-09-14 17:46:40 -0300

    ajustes em nome class  ChecInternet

---
+++ lib/app/shared/interface/check_internet_interface.dart
@@ -0,0 +1,3 @@
+abstract class ICheckInternet {  
+ Future<bool>? isInternet() {}
+}
---
+++ lib/app/shared/util/check_internet.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import '../interface/check_internet_interface.dart';
+
+class CheckInternet implements ICheckInternet {
+
+  @override
+  Future<bool>? isInternet() async {
+    bool? isNet;
+    try {
+      final result = await InternetAddress.lookup('www.google.com.br')
+          .timeout(Duration(seconds: 10));
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        debugPrint('Há internet..............');
+        isNet = true;
+      } else {
+        debugPrint('Não Há internet..............');
+        isNet = false;
+      }
+    } catch (e) {
+      debugPrint('Não Há internet..............');
+      debugPrint('Erro test net $e');
+      isNet = false;
+    }
+    return isNet;
+  }
+}
