05a963eaf620d6522b27d5324aeed025dd4723e9
correiarangel <correiarangel@bol.com.br>
2021-09-15 22:45:11 -0300

    implent test checkInternet

---
+++ test/app/shared/util/check_internet_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:my_coins/app/shared/util/check_internet.dart';
+
+class CheckInternetMock extends Mock implements CheckInternet {}
+
+void main() {
+  final checkNet = CheckInternetMock();
+
+  setUp(() {
+    print("Iniciando tests CheckInternet");
+  });
+
+  tearDown(() {
+    print("Finalizando test CheckInternet");
+  });
+
+  test('Deve retornar tipo bool true ', () async {
+    //arrage
+    bool result = true;
+    when(() => checkNet.isInternet()).thenAnswer((_) async => result);
+    //act
+    //coins = await repository.getAllCoins('USD');
+    //assert
+    expect(result, isA<bool>());
+    expect(result, true);
+  });
+
+   test('Deve retornar tipo bool false ', () async {
+    //arrage
+    bool result = false;
+    when(() => checkNet.isInternet()).thenAnswer((_) async => result);
+    //act
+    //coins = await repository.getAllCoins('USD');
+    //assert
+    expect(result, isA<bool>());
+    expect(result, false);
+  });
+   
+}
