a5aa77b548b96adca7c432ec71e1b02aa353c014
Marcos Rangel <correiarangel@bol.com.br>
2021-10-17 17:46:26 -0300

    correçao bug v inf 7.1

---
+++ lib/app/shared/services/http_overrides.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: avoid_types_on_closure_parameters
+
+import 'dart:io';
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
