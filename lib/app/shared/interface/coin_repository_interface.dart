11317b6eea4c9f748d231f4d05d9dec17ac32fcd
Marcos Rangel <correiarangel@bol.com.br>
2021-11-02 22:48:00 -0300

    implents post resp perioudo

--- lib/app/shared/interface/coin_repository_interface.dart
+++ lib/app/shared/interface/coin_repository_interface.dart
@@ -1,5 +1,7 @@
+import '../models/coins_days_model.dart';
 import '../models/coins_model.dart';
 
 abstract class ICoinRepository {
   Future<List<CoinModel>>? getAllCoins(String siglaCoin) {}
+  Future<List<CoinDaysModel>>? getPeriodCoins(String siglaCoin, String days) {}
 }
