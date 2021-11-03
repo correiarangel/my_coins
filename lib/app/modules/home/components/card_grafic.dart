e099f9a85cb20d2eec037c99a8af9e4bd2e46519
Marcos Rangel <correiarangel@bol.com.br>
2021-11-02 22:44:59 -0300

    ajustes layout

--- lib/app/modules/home/components/card_grafic.dart
+++ lib/app/modules/home/components/card_grafic.dart
@@ -3,41 +3,60 @@ import 'dart:ui';
 import 'package:chart_sparkline/chart_sparkline.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:screenshot/screenshot.dart';
 
-import '../../../shared/models/coins_model.dart';
+import '../../../shared/models/coins_days_model.dart';
 import '../../../shared/util/value/const_colors.dart';
 
 // ignore: must_be_immutable
 class CardGrafic extends StatelessWidget {
-  final List<CoinModel>? coins;
+  final ScreenshotController screenshot = ScreenshotController();
+  final List<CoinDaysModel>? coinsDays;
+
+  final String param;
   final controller;
 
   CardGrafic({
     Key? key,
-    required this.coins,
+    required this.coinsDays,
+    required this.param,
     required this.controller,
   }) : super(key: key);
 
-/*   bool get isDesktopOrWeb =>
-      Platform.isMacOS ||
-      Platform.isAndroid ||
-      Platform.isIOS ||
-      Platform.isLinux;
- */
+  List<double> data = [];
+  List<double> setData(String param) {
+    for (var i = 0; i < coinsDays!.length; i++) {
+      if (param == 'bid') data.add(double.parse("${coinsDays![i].bid}"));
+      if (param == 'ask') data.add(double.parse("${coinsDays![i].ask}"));
+      if (param == 'high') data.add(double.parse("${coinsDays![i].high}"));
+      if (param == 'low') data.add(double.parse("${coinsDays![i].low}"));
+    }
+    return data;
+  }
+
+  String getText() {
+    var days = controller.days;
+    days == "8" ? days = "7" : days;
+    var text = "";
+    if (param == 'bid') text = "Variação ultimos $days dias,\n Valor de Compra";
+    if (param == 'ask') text = "Variação ultimos $days dias,\n Valor de Venda";
+    if (param == 'high') text = "Variação ultimos $days dias,\n Valor Max.";
+    if (param == 'low') text = "Variação ultimos $days dias,\n Valor Min.";
+    return text;
+  }
+
   @override
   Widget build(BuildContext context) {
-    var data = [
-      double.parse("${coins![0].bid}"),
-      double.parse("${coins![0].ask}"),
-      double.parse("${coins![0].high}"),
-      double.parse("${coins![0].low}"),
-    ];
+    setData(param);
 
     return Padding(
       padding: EdgeInsets.only(left: 18.0, right: 18.0),
       child: InkWell(
         splashColor: Colors.purpleAccent.withAlpha(80),
-        onTap: () {},
+        onTap: () {
+          controller.changesIsNet();
+          controller.fetchcoinsDays(controller.itemSelect);
+        },
         child: Container(
           decoration: BoxDecoration(
             color: ConstColors.colorSpaceCadet,
@@ -61,30 +80,37 @@ class CardGrafic extends StatelessWidget {
               children: <Widget>[
                 SizedBox(height: 28.0),
                 Text(
-                  "Variação Compra/Venda/Max/Min:",
+                  getText(),
                   style: TextStyle(
                     color: ConstColors.colorLigthGray,
-                    fontSize: 18.0,
+                    fontSize: 22.0,
                   ),
                   textAlign: TextAlign.center,
                 ),
-                SizedBox(height: 18.0),
-                Sparkline(
-                  data: data,
-                  lineColor: ConstColors.colorLavenderFloral,
-                  pointSize: 8.0,
-                  pointColor: ConstColors.colorSkyMagenta,
-                  enableGridLines: true,
-                  averageLable: true,
-                  gridLineLabelPrecision: 4,
-                  gridLinelabelPrefix: " R\$ ",
-                  gridLineLabelColor: ConstColors.colorLigthGray,
-                  averageLine: true,
-                  pointsMode: PointsMode.all,
-                  fallbackHeight: 125.0,
-                  fillMode: FillMode.below,
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 18.0,
+                    bottom: 28.0,
+                    left: 28.0,
+                  ),
+                  child: Sparkline(
+                    data: data,
+                    lineColor: ConstColors.colorLavenderFloral,
+                    pointSize: 10.0,
+                    pointColor: ConstColors.colorSkyMagenta,
+                    enableGridLines: true,
+                    averageLable: true,
+                    gridLinelabelPrefix: " R\$ ",
+                    gridLineLabelColor: ConstColors.colorLigthGray,
+                    averageLine: true,
+                    pointsMode: PointsMode.all,
+                    fillMode: FillMode.none,
+                    enableThreshold: true,
+                    kLine: ['max', 'min', 'first', 'last'],
+                    gridLineAmount: 8,
+                    sharpCorners: true,
+                  ),
                 ),
-                SizedBox(height: 28.0),
               ],
             ),
           ),
