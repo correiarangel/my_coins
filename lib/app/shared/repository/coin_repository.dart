0c9b3cb34ff1656e0d91d3a0824cca9f04cf3ad4
Marcos Rangel <correiarangel@bol.com.br>
2021-11-02 22:51:13 -0300

    implements post periudo dias

--- lib/app/shared/repository/coin_repository.dart
+++ lib/app/shared/repository/coin_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import '../interface/coin_repository_interface.dart';
+import '../models/coins_days_model.dart';
 import '../models/coins_model.dart';
 import '../services/client_http_service.dart';
 import '../util/value/const_srtring_url.dart';
@@ -17,6 +18,26 @@ class CoinRepository implements ICoinRepository {
       Iterable interbleCoins = json.decode("[$response]");
       return listCoins = interbleCoins
           .map((comodel) => CoinModel.fromJson(comodel[siglaCoins]))
+          .toList();
+    } else {
+      return listCoins;
+    }
+  }
+
+  @override
+  Future<List<CoinDaysModel>>? getPeriodCoins(
+    String siglaCoin,
+    String days,
+  ) async {
+    var url = ConstStringUrl.urlSevenDay;
+
+    var response = await client.get("$url$siglaCoin/$days/");
+    var listCoins;
+    if (response!.statusCode == 200) {
+      response.data.removeAt(0);
+      Iterable interbleCoins = response.data;
+      return interbleCoins
+          .map((comodel) => CoinDaysModel.fromJson(comodel))
           .toList();
     } else {
       return listCoins;
