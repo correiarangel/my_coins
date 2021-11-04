f8b2d467a474059f50154d6d5ad8b0a2934dbfdb
Marcos Rangel <correiarangel@bol.com.br>
2021-11-03 21:30:09 -0300

    ajustes em Binds

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,15 +1,21 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import '../../shared/interface/coin_repository_interface.dart';
-import '../../shared/repository/coin_repository.dart';
-import 'controllers/home_store.dart';
 
+import '../../shared/interface/coin_repository_interface.dart';
+import '../../shared/interface/general_version_interface.dart';
+import '../../shared/repository/coin_repository.dart';
+import '../../shared/util/general_version.dart';
+import 'controllers/home_store.dart';
+import 'controllers/widgets_controller.dart';
 import 'home_page.dart';
+import 'interface/widgets_controller_interface.dart';
 
 class HomeModule extends Module {
   @override
   final List<Bind> binds = [
     Bind.lazySingleton((i) => HomeStore(i.get())),
-    Bind<ICoinRepository>((i) => CoinRepository(i.get()))
+    Bind<ICoinRepository>((i) => CoinRepository(i.get())),
+    Bind<IGeneralVersion>((i) => GeneralVersion()),
+    Bind<IWidGetController>((i) => WidGetController())
   ];
 
   @override
